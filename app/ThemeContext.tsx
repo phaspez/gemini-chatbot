62e01aa2813f6d62bc13d163471816893ec5af1f
Phaspez <82428635+phaspez@users.noreply.github.com>
2024-01-20 21:15:50 +0700

    fix theme again

--- app/ThemeContext.tsx
+++ app/ThemeContext.tsx
@@ -4,21 +4,30 @@ import { ThemeButtonProps } from "./GlobalInterfaces";
 const ThemeContext = createContext<ThemeButtonProps | undefined>(undefined);
 
 export const ThemeContextProvider = (props: any) => {
-	const [theme, setTheme] = useState<string>(
-		localStorage.getItem("colormode") || ""
-	);
-
+	const [theme, setTheme] = useState<string>("");
 	// get the <html> element, and or remove the dark class
 	useEffect(() => {
-		let html = document.documentElement;
-		html.classList.remove("dark");
-		html.classList.remove("light");
-
-		if (theme) {
-			html.classList.add(theme);
+		if (typeof window !== "undefined") {
+			setTheme(localStorage.getItem("colormode") || "");
+			console.log(localStorage.getItem("colormode"));
 		}
+	}, []);
 
-		localStorage.setItem("colormode", theme);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			if (!theme) {
+				return;
+			}
+			let html = document.documentElement;
+			html.classList.remove("dark");
+			html.classList.remove("light");
+
+			if (theme) {
+				html.classList.add(theme);
+			}
+
+			localStorage.setItem("colormode", theme || "");
+		}
 	}, [theme]);
 
 	const context: ThemeButtonProps = {
